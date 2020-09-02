0a7e2b3348ced2aff62786f46ebf984076392172
ZvonimirSun <hi@iszy.xyz>
2020-09-02 23:41:49 +0800

    feat(global): 调整项目配置，引入ant design vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import Antd from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(store).use(router).mount('#app')
+
+app.use(Antd)
--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,11 @@
-module.exports = {}
+module.exports = {
+    publicPath: '/',
+    outputDir: 'dist',
+    devServer: {
+        port: 3000,
+        overlay: {
+            warnings: true,
+            errors: true
+        }
+    }
+}
