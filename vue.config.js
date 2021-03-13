ffeb408ff2064c479a854680877bcfce3dd7f4c8
ZvonimirSun <hi@iszy.xyz>
2021-03-13 19:59:42 +0800

    feat(all): 调整编译规则，去除console与debugger

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer.map((arg) => {
+        const option = arg.options.terserOptions.compress
+        option.drop_console = true // 打开开关
+        option.drop_debugger = true // 打开开关
+        return arg
+      })
+    }
+  },
   devServer: {
     port: 3000,
     https: false,
