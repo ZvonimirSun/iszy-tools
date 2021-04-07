8f83c8df0e6b0c0e530c056734c5778bdfd6e4da
ZvonimirSun <hi@iszy.xyz>
2021-04-07 23:09:03 +0800

    feat(all): 切换到vite

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import 'ant-design-vue/dist/antd.css'
 import router from '@/router'
 import store from '@/store'
 import axios from 'axios'
-import Parse from 'parse'
 import { createFromIconfontCN } from '@ant-design/icons-vue'
 import '@/permission'
 
@@ -13,9 +12,6 @@ const app = createApp(App)
 const IconFont = createFromIconfontCN({
   scriptUrl: '//at.alicdn.com/t/font_1621893_lxlqg2tkrd.js'
 })
-
-Parse.serverURL = 'https://api.iszy.xyz/parse'
-Parse.initialize('3fee66ab-e2fb-463e-b0fb-8f9c11ae3a33', '66b34f1f-e00b-46af-882a-5c1e36287aae')
 
 message.config({
   duration: 2,
--- src/store/getters.js
+++
@@ -1,6 +0,0 @@
-import Parse from 'parse'
-
-const getters = {
-  currentUser: () => Parse.User.current()
-}
-export default getters
