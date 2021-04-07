fed0ef8f22074c21e6e37902674098fee698f78b
ZvonimirSun <hi@iszy.xyz>
2021-04-08 00:09:43 +0800

    feat(all): 优化vite配置

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server: { port: 3000, https: false },
+  plugins: [
+    vue(),
+    legacy()
+  ],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, '.', 'src') }
@@ -13,5 +18,20 @@ export default defineConfig({
   optimizeDeps: {
     include: ['@ant-design/icons-vue']
   },
-  base: '/'
+  build: {
+    sourcemap: false,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        dead_code: false,
+        drop_debugger: true,
+        keep_classnames: false,
+        keep_fnames: false,
+        module: false
+      },
+      keep_classnames: false,
+      keep_fnames: false,
+      module: false
+    }
+  }
 })
