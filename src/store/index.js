1247ebdfc58d6f9e20bcf81ea457b7254e0b3ab4
ZvonimirSun <hi@iszy.xyz>
2021-04-30 17:48:21 +0800

    feat(all): 添加统计信息

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,32 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  state: () => ({
+    statistics: []
+  }),
+  getters: {},
+  mutations: {
+    access (state, { name, link }) {
+      if (Array.isArray(state.statistics)) {
+        const tmp = state.statistics.filter(item => (item.name === name))
+        if (tmp.length > 0) {
+          tmp[0].times++
+          tmp[0].lastAccessTime = new Date().getTime()
+        } else {
+          state.statistics.push({
+            name, link, times: 1, lastAccessTime: new Date().getTime()
+          })
+        }
+      } else {
+        state.statistics = [{
+          name, link, times: 1, lastAccessTime: new Date().getTime()
+        }]
+      }
+    }
+  },
+  actions: {
+    access ({ commit }, { name, link }) {
+      commit({ name, link })
+    }
+  }
 })
