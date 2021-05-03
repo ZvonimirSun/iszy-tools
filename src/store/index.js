714be0993f8135d00ef39890a761ec6f78aa6dc9
ZvonimirSun <hi@iszy.xyz>
2021-05-04 01:57:34 +0800

    feat(jsonEditor): 使用vuex存储数据

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,15 @@ import { createStore } from 'vuex'
 import statistics from './modules/statistics'
 import settings from './modules/settings'
 import favorite from './modules/favorite'
+import jsonEditor from './modules/jsonEditor'
 import VuexPersist from '@/plugins/VuexPersist'
 
 export default createStore({
   modules: {
     statistics,
     settings,
-    favorite
+    favorite,
+    jsonEditor
   },
   plugins: [VuexPersist()]
 })
