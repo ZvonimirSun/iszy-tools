ce2f67bd03615c017cbdde802685389c4f9380f7
ZvonimirSun <hi@iszy.xyz>
2021-05-06 22:23:23 +0800

    pref: 调整eslint配置

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+'use strict'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
 import { VitePWA } from 'vite-plugin-pwa'
 import styleImport from 'vite-plugin-style-import'
-import path from 'path'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -72,9 +73,9 @@ export default defineConfig({
     })
   ],
   resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, '.', 'src') }
-    ]
+    alias: {
+      '@': resolve('src')
+    }
   },
   optimizeDeps: {
     include: ['@ant-design/icons-vue']
