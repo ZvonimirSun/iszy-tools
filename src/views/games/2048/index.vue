bcee1e1126a912b0e33a8f3816ff8415d1bc7502
ZvonimirSun <hi@iszy.xyz>
2021-05-18 00:33:48 +0800

    to(2048): add 2048

--- src/views/games/2048/index.vue
+++ src/views/games/2048/index.vue
@@ -1,28 +1,107 @@
 <template>
   <container>
-    <Row :gutter="16">
-      <Col :span="12">
-        <Card>
-          <Statistic title="分数" :value="score"/>
-        </Card>
-      </Col>
-      <Col :span="12"></Col>
-    </Row>
+    <div class="game-container">
+      <div class="grid-container">
+        <div class="grid-cell" v-for="item in 16" :key="item"></div>
+      </div>
+    </div>
   </container>
 </template>
 
 <script>
-import { Row, Col, Card, Statistic } from 'ant-design-vue'
 import Container from '@/components/container.vue'
+import GameManager from './js/GameManager.js'
+import KeyboardInputManager from './js/KeyboardInputManager.js'
+import HTMLActuator from './js/HTMLActuator.js'
+import LocalStorageManager from './js/LocalStorageManager.js'
+
 export default {
   name: '2048',
-  components: { Container, Row, Col, Card, Statistic },
+  components: { Container },
   data: () => ({
+    // 格网大小
+    size: 4,
+    // 初始数量
+    startTiles: 2,
+
+    cells: undefined,
+
     score: 0,
-    best: 0
-  })
+    best: 0,
+    over: false,
+    won: false,
+    keepPlaying: false
+  }),
+  mounted () {
+    window.requestAnimationFrame(function () {
+      new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager)
+    })
+  },
+  methods: {
+    setup () {
+      this.empty()
+      this.addStartTiles()
+    },
+    addStartTiles () {
+      for (const i = 0; i < this.startTiles; i++) {
+        this.addRandomTile()
+      }
+    },
+    addRandomTile () {
+      if (this.cellsAvailable()) {
+        const value = Math.random() < 0.9 ? 2 : 4
+      }
+    },
+
+    empty () {
+      this.cells = []
+      for (let x = 0; x < this.size; x++) {
+        this.cells.push([])
+        const row = this.cells[x]
+
+        for (let y = 0; y < this.size; y++) {
+          row.push(null)
+        }
+      }
+    },
+    cellsAvailable () {
+      let count = 0
+      for (const x of this.cells) {
+        for (const y of x) {
+          if (y) {
+            count++
+          }
+        }
+      }
+      return !!count
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+.game-container {
+  width: 500px;
+  height: 500px;
+  margin: 0 auto;
+  padding: 15px;
+  background: #bbada0;
+  border-radius: 6px;
+
+  .grid-container {
+    width: 100%;
+    height: 100%;
+    display: grid;
+
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+    grid-row-gap: 15px;
+    grid-column-gap: 15px;
+
+    .grid-cell {
+      border-radius: 3px;
+      background: rgba(238, 228, 218, 0.35);
+    }
+  }
+}
 </style>
---
+++ src/views/games/2048/js/KeyboardInputManager.js
@@ -0,0 +1,146 @@
+export default class KeyboardInputManager {
+  constructor () {
+    this.events = {}
+
+    if (window.navigator.msPointerEnabled) {
+      // Internet Explorer 10 style
+      this.eventTouchstart = 'MSPointerDown'
+      this.eventTouchmove = 'MSPointerMove'
+      this.eventTouchend = 'MSPointerUp'
+    } else {
+      this.eventTouchstart = 'touchstart'
+      this.eventTouchmove = 'touchmove'
+      this.eventTouchend = 'touchend'
+    }
+
+    this.listen()
+  }
+
+  on (event, callback) {
+    if (!this.events[event]) {
+      this.events[event] = []
+    }
+    this.events[event].push(callback)
+  }
+
+  emit (event, data) {
+    const callbacks = this.events[event]
+    if (callbacks) {
+      callbacks.forEach(function (callback) {
+        callback(data)
+      })
+    }
+  }
+
+  listen () {
+    const self = this
+
+    const map = {
+      38: 0, // Up
+      39: 1, // Right
+      40: 2, // Down
+      37: 3, // Left
+      75: 0, // Vim up
+      76: 1, // Vim right
+      74: 2, // Vim down
+      72: 3, // Vim left
+      87: 0, // W
+      68: 1, // D
+      83: 2, // S
+      65: 3 // A
+    }
+
+    // Respond to direction keys
+    document.addEventListener('keydown', function (event) {
+      const modifiers = event.altKey || event.ctrlKey || event.metaKey ||
+        event.shiftKey
+      const mapped = map[event.which]
+
+      if (!modifiers) {
+        if (mapped !== undefined) {
+          event.preventDefault()
+          self.emit('move', mapped)
+        }
+      }
+
+      // R key restarts the game
+      if (!modifiers && event.which === 82) {
+        self.restart.call(self, event)
+      }
+    })
+
+    // Respond to button presses
+    this.bindButtonPress('.retry-button', this.restart)
+    this.bindButtonPress('.restart-button', this.restart)
+    this.bindButtonPress('.keep-playing-button', this.keepPlaying)
+
+    // Respond to swipe events
+    let touchStartClientX, touchStartClientY
+    const gameContainer = document.getElementsByClassName('game-container')[0]
+
+    gameContainer.addEventListener(this.eventTouchstart, function (event) {
+      if ((!window.navigator.msPointerEnabled && event.touches.length > 1) ||
+        event.targetTouches.length > 1) {
+        return // Ignore if touching with more than 1 finger
+      }
+
+      if (window.navigator.msPointerEnabled) {
+        touchStartClientX = event.pageX
+        touchStartClientY = event.pageY
+      } else {
+        touchStartClientX = event.touches[0].clientX
+        touchStartClientY = event.touches[0].clientY
+      }
+
+      event.preventDefault()
+    })
+
+    gameContainer.addEventListener(this.eventTouchmove, function (event) {
+      event.preventDefault()
+    })
+
+    gameContainer.addEventListener(this.eventTouchend, function (event) {
+      if ((!window.navigator.msPointerEnabled && event.touches.length > 0) ||
+        event.targetTouches.length > 0) {
+        return // Ignore if still touching with one or more fingers
+      }
+
+      let touchEndClientX, touchEndClientY
+
+      if (window.navigator.msPointerEnabled) {
+        touchEndClientX = event.pageX
+        touchEndClientY = event.pageY
+      } else {
+        touchEndClientX = event.changedTouches[0].clientX
+        touchEndClientY = event.changedTouches[0].clientY
+      }
+
+      const dx = touchEndClientX - touchStartClientX
+      const absDx = Math.abs(dx)
+
+      const dy = touchEndClientY - touchStartClientY
+      const absDy = Math.abs(dy)
+
+      if (Math.max(absDx, absDy) > 10) {
+        // (right : left) : (down : up)
+        self.emit('move', absDx > absDy ? (dx > 0 ? 1 : 3) : (dy > 0 ? 2 : 0))
+      }
+    })
+  }
+
+  restart (event) {
+    event.preventDefault()
+    this.emit('restart')
+  }
+
+  keepPlaying (event) {
+    event.preventDefault()
+    this.emit('keepPlaying')
+  }
+
+  bindButtonPress (selector, fn) {
+    const button = document.querySelector(selector)
+    button.addEventListener('click', fn.bind(this))
+    button.addEventListener(this.eventTouchend, fn.bind(this))
+  }
+}
