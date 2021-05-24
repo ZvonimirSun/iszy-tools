1b6cb7efe51626e1ef5cd0870b98fed0197f19f8
ZvonimirSun <hi@iszy.xyz>
2021-05-25 00:30:51 +0800

    feat(store): 优化存储组织

--- src/store/modules/favorite/index.js
+++ src/store/modules/favorite/index.js
@@ -1,31 +1,69 @@
 export default {
   namespaced: true,
   state: () => ({
-    favorite: []
+    favorite: [],
+    statistics: []
   }),
   getters: {
     isFav: state => name => {
       return state.favorite.filter(item => (item.name === name)).length > 0
+    },
+    recent (state) {
+      return [...state.statistics].sort(function (a, b) {
+        return b.lastAccessTime - a.lastAccessTime
+      }).slice(0, 3)
+    },
+    most (state) {
+      return [...state.statistics].sort(function (a, b) {
+        return b.times - a.times
+      }).slice(0, 3)
     }
   },
   mutations: {
-    updateFav (state, { tool, add }) {
-      const tmp = state.favorite.filter(item => (item.name !== tool.name))
+    updateFav (state, { name, link, add }) {
+      const tmp = state.favorite.filter(item => (item.name !== name))
       if (add) {
         if (tmp.length === state.favorite.length) {
-          state.favorite.push(tool)
+          state.favorite.push({ name, link })
         }
       } else {
         state.favorite = tmp
       }
+    },
+    access (state, { name, link }) {
+      if (Array.isArray(state.statistics)) {
+        const tmp = state.statistics.filter(item => (item.name === name))
+        if (tmp.length > 0) {
+          tmp[0].times++
+          tmp[0].lastAccessTime = new Date().getTime()
+          tmp[0].link = link
+        } else {
+          state.statistics.push({
+            name, link, times: 1, lastAccessTime: new Date().getTime()
+          })
+        }
+      } else {
+        state.statistics = [{
+          name, link, times: 1, lastAccessTime: new Date().getTime()
+        }]
+      }
+    },
+    clearHistory (state) {
+      state.statistics = []
     }
   },
   actions: {
-    addFav ({ commit }, tool) {
-      commit('updateFav', { tool, add: true })
+    addFav ({ commit }, { name, link }) {
+      commit('updateFav', { name, link, add: true })
     },
-    removeFav ({ commit }, tool) {
-      commit('updateFav', { tool })
+    removeFav ({ commit }, { name, link }) {
+      commit('updateFav', { name, link })
+    },
+    access ({ commit }, { name, link }) {
+      commit('access', { name, link })
+    },
+    clearHistory ({ commit }) {
+      commit('clearHistory')
     }
   }
 }
