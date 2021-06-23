6eb620e55632e6e2bd8ad64556a06cd7e2b0a4f9
ZvonimirSun <hi@iszy.xyz>
2021-06-23 14:05:26 +0800

    feat(store): 改用indexedDb存储数据
    1. 引入localforage
    2. 调整持久化插件逻辑

--- src/plugins/VuexPersist.js
+++ src/plugins/VuexPersist.js
@@ -1,34 +1,29 @@
-import { merge } from 'lodash'
+import { merge, cloneDeep } from 'lodash'
+import localforage from 'localforage'
 
 export default (options = {}) => {
-  const storage = options.storage || (window && window.localStorage)
   const key = options.key || 'vuex'
+  localforage.config(options)
 
   // 获取state的值
-  const getState = (key, storage) => {
-    const value = storage.getItem(key)
-    try {
-      return typeof value !== 'undefined' ? JSON.parse(value) : undefined
-    } catch (err) {
-      console.warn(err)
-    }
-    return undefined
+  const getState = async (key) => {
+    return await localforage.getItem(key)
   }
 
   // 设置state的值
-  const setState = (key, state, storage) =>
-    storage.setItem(key, JSON.stringify(state))
+  const setState = (key, state) =>
+    localforage.setItem(key, cloneDeep(state))
 
-  return store => {
+  return async store => {
     // 初始化时获取数据，如果有的话，把原来的vuex的state替换掉
-    const data = merge({}, store.state, getState(key, storage))
+    const data = merge({}, store.state, await getState(key))
     if (data) {
       store.replaceState(data)
     }
 
     // 订阅 store 的 mutation。handler 会在每个 mutation 完成后调用，接收 mutation 和经过 mutation 后的状态作为参数
     store.subscribe((mutation, state) => {
-      setState(key, state, storage)
+      setState(key, state)
     })
   }
 }
