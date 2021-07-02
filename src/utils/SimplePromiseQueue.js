14ec5e7c3d6dc46ae3080256b435332cf57c38d1
ZvonimirSun <hi@iszy.xyz>
2021-07-02 20:05:25 +0800

    pref(vuex): 优化逻辑

---
+++ src/utils/SimplePromiseQueue.js
@@ -0,0 +1,24 @@
+export default class SimplePromiseQueue {
+  _queue = []
+   _flushing = false
+
+   enqueue (promise) {
+     this._queue.push(promise)
+     if (!this._flushing) { return this._flushQueue() }
+     return Promise.resolve()
+   }
+
+   _flushQueue () {
+     this._flushing = true
+
+     const chain = () => {
+       const nextTask = this._queue.shift()
+       if (nextTask) {
+         return nextTask.then(chain)
+       } else {
+         this._flushing = false
+       }
+     }
+     return Promise.resolve(chain())
+   }
+}
