07b7ef922914a77c68ee797440d2650271efd596
ZvonimirSun <hi@iszy.xyz>
2021-07-09 13:58:16 +0800

    feat(all): 添加地图选择器

--- src/utils/SimplePromiseQueue.js
+++ src/utils/SimplePromiseQueue.js
@@ -1,24 +1,25 @@
 export default class SimplePromiseQueue {
   _queue = []
-   _flushing = false
+  _flushing = false
 
-   enqueue (promise) {
-     this._queue.push(promise)
-     if (!this._flushing) { return this._flushQueue() }
-     return Promise.resolve()
-   }
+  enqueue (promise) {
+    this._queue.push(promise)
+    if (!this._flushing) { return this._flushQueue() }
+    return Promise.resolve()
+  }
 
-   _flushQueue () {
-     this._flushing = true
+  _flushQueue () {
+    this._flushing = true
 
-     const chain = () => {
-       const nextTask = this._queue.shift()
-       if (nextTask) {
-         return nextTask.then(chain)
-       } else {
-         this._flushing = false
-       }
-     }
-     return Promise.resolve(chain())
-   }
+    const chain = () => {
+      const nextTask = this._queue.shift()
+      if (nextTask) {
+        return nextTask.then(chain)
+      } else {
+        this._flushing = false
+      }
+    }
+
+    return Promise.resolve(chain())
+  }
 }
