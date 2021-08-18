9a83ec0b96ef3657eba08a3e9dc1b8a4c1dec914
ZvonimirSun <hi@iszy.xyz>
2021-08-18 16:10:54 +0800

    style(container): 调整样式

--- src/components/container.vue
+++ src/components/container.vue
@@ -1,19 +1,18 @@
 <template>
-  <Row :span="24">
-    <Col :span="24">
+  <div class="containerAll">
+    <div class="containerTitle">
       <Divider orientation="left">
-        <span class="typeName">{{name || ($route.meta || {}).title || $route.name}}&nbsp;<span class="fullScreen" @click="fullScreen"><FullscreenOutlined /></span></span>
+          <span class="typeName">{{ name || ($route.meta || {}).title || $route.name }}&nbsp;<span class="fullScreen"
+                                                                                                   @click="fullScreen"><FullscreenOutlined/></span></span>
       </Divider>
-    </Col>
-    <Col :span="24">
-      <div class="main" ref="container">
-        <slot></slot>
-      </div>
-    </Col>
-  </Row>
+    </div>
+    <div class="main" ref="container">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 <script>
-import { Row, Col, Divider } from 'ant-design-vue'
+import { Divider } from 'ant-design-vue'
 import { FullscreenOutlined } from '@ant-design/icons-vue'
 
 export default {
@@ -21,7 +20,10 @@ export default {
   props: {
     name: String
   },
-  components: { Row, Col, Divider, FullscreenOutlined },
+  components: {
+    Divider,
+    FullscreenOutlined
+  },
   methods: {
     fullScreen () {
       // eslint-disable-next-line no-useless-call
@@ -32,50 +34,51 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.ant-row {
+.containerAll {
+  height: calc(100% - 3.5rem);
   margin-top: 3.5rem;
   background: #fff;
   box-shadow: 0 0.8rem 1rem rgb(36 159 253 / 30%);
   border-radius: .8rem;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
 
-  &:not(:first-child) {
-    margin-top: 3.5rem;
-  }
+  .containerTitle {
+    margin-top: -3.5rem;
 
-  &:not(.noName) {
-    .ant-col:first-child {
-      margin-top: -3.5rem;
-    }
-  }
-}
-
-.main {
-  padding: 1.2rem;
-  background: #fff;
-}
-
-.typeName {
-  font-size: 1.8rem;
-  font-weight: 700;
-  display: inline-flex;
-  align-items: center;
-  background-color: #16b0f6;
-  padding: .48rem 1.28rem;
-  color: #fff;
-  box-shadow: 0 0.8rem 1rem rgb(36 159 253 / 30%);
-  border-radius: .8rem;
-
-  .anticon {
-    font-size: 2.4rem;
-    margin-right: .5rem;
-  }
-
-  .fullScreen {
-    cursor: pointer;
+    .typeName {
+    font-size: 1.8rem;
+    font-weight: 700;
+    display: inline-flex;
+    align-items: center;
+    background-color: #16b0f6;
+    padding: .48rem 1.28rem;
+    color: #fff;
+    box-shadow: 0 0.8rem 1rem rgb(36 159 253 / 30%);
+    border-radius: .8rem;
 
     .anticon {
-      font-size: 1.8rem;
+      font-size: 2.4rem;
+      margin-right: .5rem;
+    }
+
+    .fullScreen {
+      cursor: pointer;
+
+      .anticon {
+        font-size: 1.8rem;
+      }
     }
   }
+  }
+
+  .main {
+     padding: 1.2rem;
+     background: #fff;
+     flex: 1;
+     overflow-y: auto;
+     border-radius: .8rem;
+   }
 }
 </style>
