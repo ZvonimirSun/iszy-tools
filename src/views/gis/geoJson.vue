2bbe9ce5a06ee691a60987fec3e3f53287a9ae35
ZvonimirSun <hi@iszy.xyz>
2021-08-28 00:18:21 +0800

    to(geoJson): 恢复默认使用高德地图

--- src/views/gis/geoJson.vue
+++ src/views/gis/geoJson.vue
@@ -150,14 +150,14 @@ export default defineComponent({
         onEachFeature: this.onEachFeature
       }).addTo(this.map)
       L.control.layers({
-        OpenStreetMap: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> 贡献者'
-        }).addTo(this.map),
         高德: L.tileLayer('https://webrd0{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}', {
           subdomains: '1234',
           minZoom: 3,
           maxZoom: 18,
           attribution: '&copy; <a href="https://lbs.amap.com/pages/terms/" target="_blank">高德地图</a> 贡献者'
+        }).addTo(this.map),
+        OpenStreetMap: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> 贡献者'
         })
       }, {
         图形: toRaw(this.geoJsonLayer)
@@ -303,7 +303,7 @@ export default defineComponent({
       }
 
       .ant-table-striped .table-striped {
-        background-color: #e6e6e6;
+        background-color: #fafafa;
       }
 
     }
