04da9d8fe90990bfc14381a0ac98adbf21718330
ZvonimirSun <hi@iszy.xyz>
2021-08-31 14:49:11 +0800

    style(pwa): 调整样式

--- src/components/pwaReloadPrompt.vue
+++ src/components/pwaReloadPrompt.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    v-if="offlineReady || needRefresh"
-    class="pwa-toast"
-  >
+  <div v-if="offlineReady || needRefresh" class="pwa-toast">
     <div class="message">
       <span v-if="offlineReady">
         已经准备好离线运行~
@@ -59,17 +56,17 @@ export default defineComponent({
   text-align: left;
   box-shadow: 3px 4px 5px 0 #8885;
   background-color: white;
-}
 
-.pwa-toast .message {
-  margin-bottom: 8px;
-}
+  .message {
+    margin-bottom: 8px;
+  }
 
-.pwa-toast button {
-  border: 1px solid #8885;
-  outline: none;
-  margin-right: 5px;
-  border-radius: 2px;
-  padding: 3px 10px;
+  button {
+    border: 1px solid #8885;
+    outline: none;
+    margin-right: 5px;
+    border-radius: 2px;
+    padding: 3px 10px;
+  }
 }
 </style>
