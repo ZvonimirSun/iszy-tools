4ce4a20140e125a121bc4b5383c0b488ad3857ab
ZvonimirSun <hi@iszy.xyz>
2021-09-06 22:06:15 +0800

    pref(pwa): 调整提示逻辑

--- src/components/pwaReloadPrompt.vue
+++ src/components/pwaReloadPrompt.vue
@@ -1,72 +1,33 @@
 <template>
-  <div v-if="offlineReady || needRefresh" class="pwa-toast">
-    <div class="message">
-      <span v-if="offlineReady">
-        已经准备好离线运行~
-      </span>
-      <span v-else>
-        存在新内容，点击按钮重载来更新
-      </span>
-    </div>
-    <Button v-if="needRefresh" @click="updateServiceWorker()">
+  <Space>
+    <Button v-if="type==='reload'" @click="reload">
       重载
     </Button>
     <Button @click="close">
       关闭
     </Button>
-  </div>
+  </Space>
 </template>
 <script>
-import { useRegisterSW } from 'virtual:pwa-register/vue'
 import { defineComponent } from 'vue'
-import { Button } from 'ant-design-vue'
-
-const {
-  offlineReady,
-  needRefresh,
-  updateServiceWorker
-} = useRegisterSW()
+import { Button, Space } from 'ant-design-vue'
 
 export default defineComponent({
   name: 'pwaReloadPrompt',
-  components: { Button },
-  data: () => ({
-    offlineReady,
-    needRefresh
-  }),
+  components: { Button, Space },
+  props: {
+    type: {
+      type: String
+    }
+  },
+  emits: ['close', 'reload'],
   methods: {
-    updateServiceWorker,
-    async close () {
-      this.offlineReady = false
-      this.needRefresh = false
+    reload () {
+      this.$emit('reload')
+    },
+    close () {
+      this.$emit('close')
     }
   }
 })
 </script>
-<style lang="scss" scoped>
-.pwa-toast {
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  margin: 16px;
-  padding: 12px;
-  border: 1px solid #8885;
-  border-radius: 4px;
-  z-index: 1;
-  text-align: left;
-  box-shadow: 3px 4px 5px 0 #8885;
-  background-color: white;
-
-  .message {
-    margin-bottom: 8px;
-  }
-
-  button {
-    border: 1px solid #8885;
-    outline: none;
-    margin-right: 5px;
-    border-radius: 2px;
-    padding: 3px 10px;
-  }
-}
-</style>
