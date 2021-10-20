f9e30c95a1f00eeedafcda1e82df47b90874c10f
ZvonimirSun <hi@iszy.xyz>
2021-10-20 14:52:13 +0800

    to(3dView): 尝试加载本地gltf

--- src/views/others/3dView/3dView.vue
+++ src/views/others/3dView/3dView.vue
@@ -1,5 +1,15 @@
 <template>
   <container>
+    <Upload :fileList="[]" :showUploadList="false" :before-upload="upload">
+      <Input readonly placeholder="点击这里上传文件" :value="fileName">
+        <template #addonAfter>
+          <Button block :disabled="!file" :loading="loading" @click.stop="load">
+            <span v-if="loading">加载中</span>
+            <span v-else>加载</span>
+          </Button>
+        </template>
+      </Input>
+    </Upload>
     <div class="threeContainer" ref="treeContainer"></div>
   </container>
 </template>
@@ -9,6 +19,7 @@ import { Container } from '@/components'
 import { WebGLRenderer, PerspectiveCamera, AmbientLight, Scene, Color } from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { Input, Upload, Button } from 'ant-design-vue'
 
 let scene, camera, renderer, ambientLight, controls, loader, modelScene
 
@@ -20,41 +31,64 @@ const animate = () => {
 
 export default {
   name: '3dView',
-  components: { Container },
+  components: { Container, Upload, Input, Button },
+  data: () => ({
+    file: undefined,
+    fileName: undefined,
+    loading: false
+  }),
   mounted () {
-    // Scene
-    scene = new Scene()
-    scene.background = new Color(0x999999)
-
-    // Renderer
-    renderer = new WebGLRenderer({ antialias: true })
-    renderer.setSize(parseFloat(window.getComputedStyle(this.$refs.treeContainer).width), parseFloat(window.getComputedStyle(this.$refs.treeContainer).height))
-    this.$refs.treeContainer.appendChild(renderer.domElement)
-
-    // Camera
-    const aspect = parseFloat(window.getComputedStyle(this.$refs.treeContainer).width) / parseFloat(window.getComputedStyle(this.$refs.treeContainer).height)
-    camera = new PerspectiveCamera(60, aspect, 0.01, 5000)
-    camera.position.set(5, 2, 0)
-    renderer.render(scene, camera)
-
-    // Camera Controls
-    controls = new OrbitControls(camera, renderer.domElement)
-
-    // Light
-    ambientLight = new AmbientLight(0xaaaaaa, 20)
-    scene.add(ambientLight)
-
-    loader = new GLTFLoader()
-    // this.$nextTick(() => {
-    //   // Loader
-    //   loader = new GLTFLoader()
-    //   loader.load('/models/lamborghini_huracan_gt3_-_squadra_corse_sdc/scene.gltf', result => {
-    //     scene.add(result.scene)
-    //     animate()
-    //   })
-    // })
+    this.initScene()
   },
   methods: {
+    initScene () {
+      // Scene
+      scene = new Scene()
+      scene.background = new Color(0x999999)
+
+      // Renderer
+      renderer = new WebGLRenderer({ antialias: true })
+      renderer.setSize(parseFloat(window.getComputedStyle(this.$refs.treeContainer).width), parseFloat(window.getComputedStyle(this.$refs.treeContainer).height))
+      this.$refs.treeContainer.appendChild(renderer.domElement)
+
+      // Camera
+      const aspect = parseFloat(window.getComputedStyle(this.$refs.treeContainer).width) / parseFloat(window.getComputedStyle(this.$refs.treeContainer).height)
+      camera = new PerspectiveCamera(60, aspect, 0.01, 5000)
+      camera.position.set(5, 2, 0)
+      renderer.render(scene, camera)
+
+      // Camera Controls
+      controls = new OrbitControls(camera, renderer.domElement)
+
+      // Light
+      ambientLight = new AmbientLight(0xaaaaaa, 20)
+      scene.add(ambientLight)
+
+      // Loader
+      loader = new GLTFLoader()
+    },
+    upload (file) {
+      if (this.file) {
+        URL.revokeObjectURL(this.file)
+        this.file = undefined
+      }
+      this.fileName = file.name
+      this.file = URL.createObjectURL(file)
+      return false
+    },
+    load () {
+      this.loading = true
+      if (modelScene) {}
+      if (this.file) {
+        loader.load(this.file, result => {
+          modelScene = scene.add(result.scene)
+          animate()
+          this.loading = false
+        }, () => {}, () => {
+          this.loading = false
+        })
+      }
+    }
   }
 }
 </script>
@@ -62,6 +96,16 @@ export default {
 <style scoped lang="scss">
 .threeContainer {
   width: 100%;
-  height: 100%;
+  height: calc(100% - .8rem - 32px);
+}
+
+:deep(.ant-upload) {
+  width: 100% !important;
+  margin-bottom: .8rem;
+}
+
+:deep(.ant-input-group-addon) {
+  padding: 0;
+  border: unset;
 }
 </style>
