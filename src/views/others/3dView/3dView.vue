0dcbbea65fffa961414fdce3fafde054a3735b07
ZvonimirSun <hi@iszy.xyz>
2021-10-21 10:43:49 +0800

    feat(3dView): 展示gltf文件

--- src/views/others/3dView/3dView.vue
+++ src/views/others/3dView/3dView.vue
@@ -1,9 +1,9 @@
 <template>
   <container>
-    <Upload :fileList="[]" :showUploadList="false" :before-upload="upload">
-      <Input readonly placeholder="点击这里上传文件" :value="fileName">
+    <Upload :fileList="[]" :showUploadList="false" :before-upload="getFileList" :directory="true">
+      <Input readonly placeholder="点击这里选择 gltf 文件所在文件夹" :value="fileName">
         <template #addonAfter>
-          <Button block :disabled="!file" :loading="loading" @click.stop="load">
+          <Button block :disabled="!fileName" :loading="loading" @click.stop="loadScene">
             <span v-if="loading">加载中</span>
             <span v-else>加载</span>
           </Button>
@@ -16,12 +16,26 @@
 
 <script>
 import { Container } from '@/components'
-import { WebGLRenderer, PerspectiveCamera, AmbientLight, Scene, Color } from 'three'
+import { WebGLRenderer, PerspectiveCamera, AmbientLight, Scene, Color, LoaderUtils, LoadingManager } from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { Input, Upload, Button } from 'ant-design-vue'
 
-let scene, camera, renderer, ambientLight, controls, loader, modelScene
+let scene, camera, renderer, ambientLight, controls, manager
+let fileList, fileMap, rootFile, rootPath, loader, modelScene
+let blobURLs = []
+// glTF texture types. `envMap` is deliberately omitted, as it's used internally
+// by the loader but not part of the glTF format.
+const MAP_NAMES = [
+  'map',
+  'aoMap',
+  'emissiveMap',
+  'glossinessMap',
+  'metalnessMap',
+  'normalMap',
+  'roughnessMap',
+  'specularMap'
+]
 
 const animate = () => {
   requestAnimationFrame(animate)
@@ -31,9 +45,13 @@ const animate = () => {
 
 export default {
   name: '3dView',
-  components: { Container, Upload, Input, Button },
+  components: {
+    Container,
+    Upload,
+    Input,
+    Button
+  },
   data: () => ({
-    file: undefined,
     fileName: undefined,
     loading: false
   }),
@@ -64,30 +82,76 @@ export default {
       ambientLight = new AmbientLight(0xaaaaaa, 20)
       scene.add(ambientLight)
 
-      // Loader
-      loader = new GLTFLoader()
+      manager = new LoadingManager()
+      animate()
     },
-    upload (file) {
-      if (this.file) {
-        URL.revokeObjectURL(this.file)
-        this.file = undefined
+    getFileList (f, list) {
+      if (fileList !== list) {
+        const map = new Map()
+        let flag = 0
+        for (const file of list) {
+          const path = file.webkitRelativePath || file.name
+          map.set(path, file)
+          if (file.name.match(/\.(gltf|glb)$/)) {
+            flag = 1
+            this.fileName = file.name
+            rootFile = file
+            rootPath = path.replace(file.name, '')
+          }
+        }
+        if (flag === 1) {
+          fileMap = map
+        } else {
+          this.$msg.error('此文件夹不包含gltf文件')
+        }
       }
-      this.fileName = file.name
-      this.file = URL.createObjectURL(file)
       return false
     },
-    load () {
-      this.loading = true
-      if (modelScene) {}
-      if (this.file) {
-        loader.load(this.file, result => {
-          modelScene = scene.add(result.scene)
-          animate()
-          this.loading = false
-        }, () => {}, () => {
-          this.loading = false
-        })
+    loadScene () {
+      this.clearScene()
+      const fileURL = typeof rootFile === 'string'
+        ? rootFile
+        : URL.createObjectURL(rootFile)
+      const baseURL = LoaderUtils.extractUrlBase(fileURL)
+      manager.setURLModifier((url, path) => {
+        const normalizedURL = rootPath + decodeURI(url)
+          .replace(baseURL, '')
+          .replace(/^(\.?\/)/, '')
+        if (fileMap.has(normalizedURL)) {
+          const blob = fileMap.get(normalizedURL)
+          const blobURL = URL.createObjectURL(blob)
+          blobURLs.push(blobURL)
+          return blobURL
+        }
+        return (path || '') + url
+      })
+      blobURLs = []
+      loader = new GLTFLoader(manager)
+      loader.load(fileURL, (gltf) => {
+        modelScene = gltf.scene || gltf.scenes[0]
+        scene.add(modelScene)
+        blobURLs.forEach(URL.revokeObjectURL)
+      }, () => {}, () => {
+        blobURLs.forEach(URL.revokeObjectURL)
+      })
+    },
+    clearScene () {
+      if (!modelScene) {
+        return
       }
+      scene.remove(modelScene)
+      modelScene.traverse((node) => {
+        if (!node.isMesh) return
+        node.geometry.dispose()
+        const materials = Array.isArray(node.material)
+          ? node.material
+          : [node.material]
+        materials.forEach((material) => {
+          MAP_NAMES.forEach((map) => {
+            if (material[map]) material[map].dispose()
+          })
+        })
+      })
     }
   }
 }
