6aeb8634d1600680b6008b362bec0ee8013c5564
ZvonimirSun <hi@iszy.xyz>
2021-11-04 14:10:37 +0800

    chore: 拆分随机数功能

--- src/views/others/random/random.vue
+++ src/views/others/random/random.vue
@@ -2,29 +2,7 @@
   <container>
     <Tabs>
       <TabPane key="common" tab="通用">
-        <Form :labelCol="{sm:{span:2}}" :wrapperCol="{sm:{span:3}}">
-          <FormItem label="最小数字">
-            <InputNumber v-model:value.number="min"/>
-          </FormItem>
-          <FormItem label="最大数字">
-            <InputNumber v-model:value.number="max"/>
-          </FormItem>
-          <FormItem label="次数">
-            <InputNumber v-model:value.number="times" :step="1"/>
-          </FormItem>
-          <FormItem label="小数">
-            <Switch v-model:checked="float"/>
-          </FormItem>
-          <FormItem :wrapperCol="{sm:{span:3,offset:2}}">
-            <Space>
-              <Button type="primary" @click="randomNum">生成</Button>
-              <Button @click="clearRandomNum">清空</Button>
-            </Space>
-          </FormItem>
-          <FormItem :wrapperCol="{sm:{span:5}}">
-            <TextArea :value="randomNumResult" readonly :auto-size="{ minRows: 2, maxRows: 5 }"/>
-          </FormItem>
-        </Form>
+        <RandomNumber/>
       </TabPane>
       <TabPane key="advanced" tab="高级"></TabPane>
     </Tabs>
@@ -33,53 +11,21 @@
 
 <script>
 import Container from '@/components/container.vue'
-import { Tabs, Form, InputNumber, Switch, Button, Input, Space } from 'ant-design-vue'
-import { random } from 'lodash-es'
+import { Tabs } from 'ant-design-vue'
+import { defineAsyncComponent, defineComponent } from 'vue'
 
 const { TabPane } = Tabs
-const { Item: FormItem } = Form
-const { TextArea } = Input
 
-export default {
+export default defineComponent({
   name: 'random',
   components: {
+    RandomNumber: defineAsyncComponent(() => import('@/views/others/random/randomNumber.vue')),
     Container,
     Tabs,
-    TabPane,
-    Form,
-    FormItem,
-    InputNumber,
-    Switch,
-    Button,
-    TextArea,
-    Space
-  },
-  data: () => ({
-    min: 0,
-    max: 10,
-    times: 1,
-    float: false,
-    randomNumResult: ''
-  }),
-  methods: {
-    randomNum () {
-      try {
-        for (let i = 0; i < this.times; i++) {
-          this.randomNumResult = random(this.min, this.max, this.float) + (this.randomNumResult === '' ? '' : '\n') + this.randomNumResult
-        }
-      } catch (e) {
-        this.$msg.error('计算错误')
-      }
-    },
-    clearRandomNum () {
-      this.randomNumResult = ''
-    }
+    TabPane
   }
-}
+})
 </script>
 
 <style scoped lang="scss">
-.ant-input-number {
-  width: 100%;
-}
 </style>
