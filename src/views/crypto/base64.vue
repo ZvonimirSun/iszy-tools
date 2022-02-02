fc4a9471bf305d7d10697afdde334c38eca18c02
ZvonimirSun <hi@iszy.xyz>
2022-02-03 00:02:25 +0800

    chore: 调整base64编码方式

--- src/views/crypto/base64.vue
+++ src/views/crypto/base64.vue
@@ -12,21 +12,28 @@
 
 <script setup>
 import { Input, Space, Button } from 'ant-design-vue'
-import { ref } from 'vue'
-import Base64 from 'crypto-js/enc-base64.js'
-import Utf8 from 'crypto-js/enc-utf8.js'
+import { ref, inject } from 'vue'
 
 const { TextArea } = Input
 
 const data1 = ref('')
 const data2 = ref('')
+const msg = inject('$msg')
 
 function encode () {
-  data2.value = Base64.stringify(Utf8.parse(data1.value))
+  data2.value = btoa(encodeURIComponent(data1.value).replace(/%([0-9A-F]{2})/g, function (match, p1) {
+    return String.fromCharCode('0x' + p1)
+  }))
 }
 
 function decode () {
-  data2.value = Base64.parse(data1.value).toString(Utf8)
+  try {
+    data2.value = decodeURIComponent(atob(data1.value).split('').map(function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+    }).join(''))
+  } catch (e) {
+    msg.error('解码失败')
+  }
 }
 
 function exchange () {
