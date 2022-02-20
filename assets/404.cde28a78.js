e4c72bfd64fe3e97e99fa852e20952ee87ff0b10
ZvonimirSun <acc@iszy.xyz>
2022-02-20 08:29:57 +0000

    Deploying to deploy from @ ZvonimirSun/iszy-tools@b4fa740ae9e7899e2fb2f583c3c53f39f82785a4 🚀

---
+++ assets/404.cde28a78.js
@@ -0,0 +1,1 @@
+import{h as t,u as e,a2 as s,o as a}from"./vendor.53be07e2.js";/* empty css              */const l={setup(r){return(o,u)=>(a(),t(e(s),{status:"404",title:"404","sub-title":"\u5BF9\u4E0D\u8D77\uFF0C\u4F60\u8BBF\u95EE\u7684\u9875\u9762\u4E0D\u5B58\u5728\u3002"}))}};export{l as default};
