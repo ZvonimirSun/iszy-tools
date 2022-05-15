e7875beeefe4420bc02414eef5ab18dae879e186
ZvonimirSun <hi@iszy.xyz>
2022-05-15 20:16:53 +0800

    feat(colorTransform): 支持cmyk

--- src/views/conversion/colorTransform.vue
+++ src/views/conversion/colorTransform.vue
@@ -57,6 +57,7 @@
               :max="255"
               :min="0"
               :step="1"
+              :precision="0"
               @change="inputChange({rgba})"
             />
             <a-input-number
@@ -65,6 +66,7 @@
               :max="255"
               :min="0"
               :step="1"
+              :precision="0"
               @change="inputChange({rgba})"
             />
             <a-input-number
@@ -73,6 +75,7 @@
               :max="255"
               :min="0"
               :step="1"
+              :precision="0"
               @change="inputChange({rgba})"
             />
             <a-input-number
@@ -81,7 +84,120 @@
               :max="1"
               :min="0"
               :step="0.01"
+              :precision="2"
               @change="inputChange({rgba})"
+            />
+          </div>
+        </a-form-item>
+        <a-form-item
+          label="CMYK"
+          class="cmyk"
+        >
+          <div
+            flex
+            w-full
+            justify-between
+            overflow-auto
+            class="gap-3.2"
+          >
+            <a-input-number
+              v-model:value="cmyk.c"
+              placeholder="C"
+              :max="1"
+              :min="0"
+              :step="0.01"
+              :formatter="formatterPercentage"
+              :parser="parserPercentage"
+              :precision="2"
+              @change="inputChange({cmyk})"
+            />
+            <a-input-number
+              v-model:value="cmyk.m"
+              placeholder="M"
+              :max="1"
+              :min="0"
+              :step="0.01"
+              :formatter="formatterPercentage"
+              :parser="parserPercentage"
+              :precision="2"
+              @change="inputChange({cmyk})"
+            />
+            <a-input-number
+              v-model:value="cmyk.y"
+              placeholder="Y"
+              :max="1"
+              :min="0"
+              :step="0.01"
+              :formatter="formatterPercentage"
+              :parser="parserPercentage"
+              :precision="2"
+              @change="inputChange({cmyk})"
+            />
+            <a-input-number
+              v-model:value="cmyk.k"
+              placeholder="K"
+              :max="1"
+              :min="0"
+              :step="0.01"
+              :formatter="formatterPercentage"
+              :parser="parserPercentage"
+              :precision="2"
+              @change="inputChange({cmyk})"
+            />
+          </div>
+        </a-form-item>
+        <a-form-item
+          label="HSVA"
+          class="hsva"
+        >
+          <div
+            flex
+            w-full
+            justify-between
+            overflow-auto
+            class="gap-3.2"
+          >
+            <a-input-number
+              v-model:value="hsva.h"
+              placeholder="H"
+              :max="360"
+              :min="0"
+              :step="0.01"
+              :formatter="formatterDegree"
+              :parser="parserDegree"
+              :precision="2"
+              @change="inputChange({hsva})"
+            />
+            <a-input-number
+              v-model:value="hsva.s"
+              placeholder="S"
+              :max="1"
+              :min="0"
+              :step="0.01"
+              :formatter="formatterPercentage"
+              :parser="parserPercentage"
+              :precision="2"
+              @change="inputChange({hsva})"
+            />
+            <a-input-number
+              v-model:value="hsva.v"
+              placeholder="V"
+              :max="1"
+              :min="0"
+              :step="0.01"
+              :formatter="formatterPercentage"
+              :parser="parserPercentage"
+              :precision="2"
+              @change="inputChange({hsva})"
+            />
+            <a-input-number
+              v-model:value="hsva.a"
+              placeholder="A"
+              :max="1"
+              :min="0"
+              :step="0.01"
+              :precision="2"
+              @change="inputChange({hsva})"
             />
           </div>
         </a-form-item>
@@ -102,6 +218,8 @@
               :max="360"
               :min="0"
               :step="0.01"
+              :formatter="formatterDegree"
+              :parser="parserDegree"
               :precision="2"
               @change="inputChange({hsla})"
             />
@@ -138,62 +256,6 @@
             />
           </div>
         </a-form-item>
-        <a-form-item
-          label="HSVA"
-          class="hsva"
-        >
-          <div
-            flex
-            w-full
-            justify-between
-            overflow-auto
-            class="gap-3.2"
-          >
-            <a-input-number
-              v-model:value="hsva.h"
-              placeholder="H"
-              :max="360"
-              :min="0"
-              :step="0.01"
-              :precision="2"
-              w-full
-              @change="inputChange({hsva})"
-            />
-            <a-input-number
-              v-model:value="hsva.s"
-              placeholder="S"
-              :max="1"
-              :min="0"
-              :step="0.01"
-              :formatter="formatterPercentage"
-              :parser="parserPercentage"
-              :precision="2"
-              w-full
-              @change="inputChange({hsva})"
-            />
-            <a-input-number
-              v-model:value="hsva.v"
-              placeholder="V"
-              :max="1"
-              :min="0"
-              :step="0.01"
-              :formatter="formatterPercentage"
-              :parser="parserPercentage"
-              :precision="2"
-              w-full
-              @change="inputChange({hsva})"
-            />
-            <a-input-number
-              v-model:value="hsva.a"
-              placeholder="A"
-              :max="1"
-              :min="0"
-              :step="0.01"
-              :precision="2"
-              @change="inputChange({hsva})"
-            />
-          </div>
-        </a-form-item>
       </a-form>
     </div>
     <div
@@ -217,7 +279,7 @@
         >
           <Saturation
             :value="colors"
-            @change="childChange"
+            @change="colorChange"
           />
         </div>
         <div
@@ -226,7 +288,7 @@
         >
           <Hue
             :value="colors"
-            @change="childChange"
+            @change="colorChange"
           />
         </div>
         <div
@@ -235,7 +297,7 @@
         >
           <Alpha
             :value="colors"
-            @change="childChange"
+            @change="colorChange"
           />
         </div>
       </a-space>
@@ -262,13 +324,19 @@
 <script setup>
 import tinyColor from 'tinycolor2'
 
-const cssString = ref('#16b0f6ff')
+const cssString = ref('#16b0f6')
 const hex = ref('')
 const rgba = ref({
   r: undefined,
   g: undefined,
   b: undefined,
   a: undefined
+})
+const cmyk = ref({
+  c: undefined,
+  m: undefined,
+  y: undefined,
+  k: undefined
 })
 const hsla = ref({
   h: undefined,
@@ -293,12 +361,53 @@ onMounted(() => {
   inputChange({ cssString: cssString.value })
 })
 
-function handleCssString () {
-  const ctx = document.createElement('canvas').getContext('2d')
-  ctx.fillStyle = cssString.value || ''
-  colorChange(ctx.fillStyle)
+function inputChange (data) {
+  if (!data) {
+    return
+  }
+  if (data.hex) {
+    isValidColor(data.hex) && colorChange({
+      hex: data.hex,
+      from: 'hex'
+    })
+  } else if (data.rgba) {
+    isValidColor(data.rgba) && colorChange({
+      r: data.rgba.r || colors.value.rgba.r,
+      g: data.rgba.g || colors.value.rgba.g,
+      b: data.rgba.b || colors.value.rgba.b,
+      a: data.rgba.a || colors.value.rgba.a,
+      from: 'rgba'
+    })
+  } else if (data.hsla) {
+    const tmp = `hsla(${data.hsla.h},${(data.hsla.s * 100).toFixed(0)}%,${(data.hsla.l * 100).toFixed(0)}%,${data.hsla.a}`
+    isValidColor(tmp) && colorChange({
+      hsl: tmp,
+      from: 'hsla'
+    })
+  } else if (data.hsva) {
+    const tmp = `hsva(${data.hsva.h},${(data.hsva.s * 100).toFixed(0)}%,${(data.hsva.v * 100).toFixed(0)}%,${data.hsva.a}`
+    isValidColor(tmp) && colorChange({
+      hsv: tmp,
+      from: 'hsva'
+    })
+  } else if (data.cssString) {
+    isValidColor(data.cssString) && colorChange({ cssString: data.cssString, from: 'cssString' })
+  } else if (data.cmyk) {
+    colorChange({
+      cmyk: data.cmyk,
+      from: 'cmyk'
+    })
+  }
 }
-function colorChange (data, isCssString = false) {
+function colorChange (data) {
+  if (data.from === 'cmyk') {
+    const rgbLocal = cmyk2rgb(cmyk.value.c, cmyk.value.m, cmyk.value.y, cmyk.value.k)
+    if (rgbLocal) {
+      data.rgba = `rgb(${rgbLocal[0]},${rgbLocal[1]},${rgbLocal[2]})`
+    } else {
+      return
+    }
+  }
   const {
     rgba: rgbaLocal,
     hex: hexLocal,
@@ -307,24 +416,39 @@ function colorChange (data, isCssString = false) {
     hsl,
     hsv
   } = colors.value = getNormalizedColor(data, colors.value.hsl.h)
-  rgba.value.r = rgbaLocal.r
-  rgba.value.g = rgbaLocal.g
-  rgba.value.b = rgbaLocal.b
-  rgba.value.a = rgbaLocal.a
-  hsla.value.h = hsl.h
-  hsla.value.s = hsl.s
-  hsla.value.l = hsl.l
-  hsla.value.a = a
-  hsva.value.h = hsv.h
-  hsva.value.s = hsv.s
-  hsva.value.v = hsv.v
-  hsva.value.a = a
+  if (data.from !== 'rgba') {
+    rgba.value.r = rgbaLocal.r
+    rgba.value.g = rgbaLocal.g
+    rgba.value.b = rgbaLocal.b
+    rgba.value.a = rgbaLocal.a
+  }
+  if (data.from !== 'hsla') {
+    hsla.value.h = hsl.h
+    hsla.value.s = hsl.s
+    hsla.value.l = hsl.l
+    hsla.value.a = a
+  }
+  if (data.from !== 'hsva') {
+    hsva.value.h = hsv.h
+    hsva.value.s = hsv.s
+    hsva.value.v = hsv.v
+    hsva.value.a = a
+  }
+  if (data.from !== 'cmyk') {
+    const cmykLocal = rgb2cmyk(rgbaLocal.r, rgbaLocal.g, rgbaLocal.b)
+    if (cmykLocal) {
+      cmyk.value.c = cmykLocal[0]
+      cmyk.value.m = cmykLocal[1]
+      cmyk.value.y = cmykLocal[2]
+      cmyk.value.k = cmykLocal[3]
+    }
+  }
   if (a === 1) {
     hex.value = hexLocal.replace('#', '')
   } else {
     hex.value = hex8.replace('#', '')
   }
-  if (!isCssString) {
+  if (data.from !== 'cssString') {
     if (a === 1) {
       cssString.value = '#' + hex.value.toLowerCase()
     } else {
@@ -332,46 +456,23 @@ function colorChange (data, isCssString = false) {
     }
   }
 }
-function isValidColor (color) {
-  return tinyColor(color).isValid()
-}
-function childChange (data) {
-  colorChange(data)
-}
-function inputChange (data) {
-  if (!data) {
-    return
-  }
-  if (data.hex) {
-    isValidColor(data.hex) && colorChange({
-      hex: data.hex,
-      source: 'hex'
-    })
-  } else if (data.rgba) {
-    isValidColor(data.rgba) && colorChange({
-      r: data.rgba.r || colors.value.rgba.r,
-      g: data.rgba.g || colors.value.rgba.g,
-      b: data.rgba.b || colors.value.rgba.b,
-      a: data.rgba.a || colors.value.rgba.a,
-      source: 'rgba'
-    })
-  } else if (data.hsla) {
-    const tmp = `hsla(${data.hsla.h},${(data.hsla.s * 100).toFixed(0)}%,${(data.hsla.l * 100).toFixed(0)}%,${data.hsla.a}`
-    isValidColor(tmp) && colorChange(tmp)
-  } else if (data.hsva) {
-    const tmp = `hsva(${data.hsva.h},${(data.hsva.s * 100).toFixed(0)}%,${(data.hsva.v * 100).toFixed(0)}%,${data.hsva.a}`
-    isValidColor(tmp) && colorChange(tmp)
-  } else if (data.cssString) {
-    isValidColor(data.cssString) && colorChange(data.cssString, true)
-  }
-}
+
 function formatterPercentage (value) {
   return `${(value * 100).toFixed(0)}%`
 }
 function parserPercentage (value) {
-  return parseInt(value.replace('%', '')) / 100
+  return parseFloat((parseInt(value.replace('%', '')) / 100).toFixed(2))
+}
+function formatterDegree (value) {
+  return `${parseFloat(parseFloat(value).toFixed(2))}°`
+}
+function parserDegree (value) {
+  return parseFloat(value.replace('°', ''))
 }
 
+function isValidColor (color) {
+  return tinyColor(color).isValid()
+}
 function getNormalizedColor (data, oldHue) {
   const alpha = data && data.a
   let color
@@ -387,6 +488,8 @@ function getNormalizedColor (data, oldHue) {
     color = tinyColor(data.rgba)
   } else if (data && data.rgb) {
     color = tinyColor(data.rgb)
+  } else if (data && data.cssString) {
+    color = tinyColor(data.cssString)
   } else {
     color = tinyColor(data)
   }
@@ -409,9 +512,70 @@ function getNormalizedColor (data, oldHue) {
     rgba: color.toRgb(),
     hsv,
     oldHue: data.h || oldHue || hsl.h,
-    source: data.source,
+    from: data.from,
     a: data.a || color.getAlpha()
   }
+}
+function rgb2cmyk (r, g, b) {
+  let computedC = 0
+  let computedM = 0
+  let computedY = 0
+  let computedK = 0
+
+  // remove spaces from input RGB values, convert to int
+  r = parseInt(('' + r).replace(/\s/g, ''), 10)
+  g = parseInt(('' + g).replace(/\s/g, ''), 10)
+  b = parseInt(('' + b).replace(/\s/g, ''), 10)
+
+  if (r == null || g == null || b == null ||
+    isNaN(r) || isNaN(g) || isNaN(b)) {
+    return
+  }
+  if (r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255) {
+    return
+  }
+
+  // BLACK
+  if (r === 0 && g === 0 && b === 0) {
+    return [0, 0, 0, 1]
+  }
+
+  computedC = 1 - (r / 255)
+  computedM = 1 - (g / 255)
+  computedY = 1 - (b / 255)
+
+  const minCMY = Math.min(computedC, Math.min(computedM, computedY))
+  computedC = (computedC - minCMY) / (1 - minCMY)
+  computedM = (computedM - minCMY) / (1 - minCMY)
+  computedY = (computedY - minCMY) / (1 - minCMY)
+  computedK = minCMY
+
+  return [computedC, computedM, computedY, computedK]
+}
+function cmyk2rgb (c, m, y, k) {
+  if (c == null || m == null || y == null || k == null) {
+    return
+  }
+  if (isNaN(c) || isNaN(m) || isNaN(y) || isNaN(k)) {
+    return
+  }
+  if (c < 0 || m < 0 || y < 0 || k < 0 || c > 1 || m > 1 || y > 1 || k > 1) {
+    return
+  }
+
+  let r
+  let g
+  let b
+
+  r = 255 * (1 - c) * (1 - k)
+  g = 255 * (1 - m) * (1 - k)
+  b = 255 * (1 - y) * (1 - k)
+
+  r = Math.round(r)
+  g = Math.round(g)
+  b = Math.round(b)
+
+  return [r, g, b]
 }
 </script>
 
