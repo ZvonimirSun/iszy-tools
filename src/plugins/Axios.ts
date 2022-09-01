87433b9f1a1f9852db8cae4617bb0d2623e7627a
ZvonimirSun <hi@iszy.xyz>
2022-09-01 08:29:30 +0800

    feat: 修复401逻辑

--- src/plugins/Axios.ts
+++ src/plugins/Axios.ts
@@ -1,6 +1,5 @@
-import Axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+import Axios, { AxiosResponse } from 'axios'
 import store from '@/store'
-import router from '@/router'
 
 const axiosInstance = Axios.create()
 axiosInstance.$apiBase = ''
@@ -8,11 +7,8 @@ axiosInstance.CancelToken = Axios.CancelToken
 axiosInstance.isCancel = Axios.isCancel
 axiosInstance.interceptors.response.use(
   (response: AxiosResponse) => {
-    // todo 修复401逻辑
-    if (response.config.url.includes(axiosInstance.$apiBase)) {
-      if (response.data && response.data.code === 'A0401') {
-        router.push('/403')
-      }
+    if (response.status === 401 && response.config.url.includes(axiosInstance.$apiBase)) {
+      store.dispatch('user/logout')
     }
     return response
   }, async error => {
