9caad5cc851ce40f5b60146b5fd44827ed6fe050
ZvonimirSun <hi@iszy.xyz>
2022-09-19 13:54:10 +0800

    feat: 引入fundebug

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 import axios from '@/plugins/Axios'
+import * as fundebug from 'fundebug-javascript'
+import FundebugVue from 'fundebug-vue'
 import 'ant-design-vue/es/message/style/css.js'
+
+const fundebugApiKey = '376d597c08f80d72c91b09f49055384c76742c47c07783dadbad8d196c318d0c'
+const $apiBase = 'https://api.iszy.xyz'
 
 const app = createApp(App)
 
@@ -14,21 +19,24 @@ message.config({
 })
 
 axios.defaults.withCredentials = true
-
 app.config.globalProperties.$axios = axios
 app.config.globalProperties.$msg = message
 app.config.globalProperties.$eventBus = EventBus
-app.config.globalProperties.$apiBase = 'https://api.iszy.xyz'
+app.config.globalProperties.$apiBase = $apiBase
 
 app.provide('$axios', axios)
 app.provide('$msg', message)
 app.provide('$eventBus', EventBus)
-app.provide('$apiBase', 'https://api.iszy.xyz')
+app.provide('$apiBase', $apiBase)
 
 store.$axios = app.config.globalProperties.$axios
 store.$msg = app.config.globalProperties.$msg
 store.$apiBase = app.config.globalProperties.$apiBase
 axios.$apiBase = app.config.globalProperties.$apiBase
 
-app.use(store).use(router)
+fundebug.init({
+  apikey: fundebugApiKey
+})
+
+app.use(new FundebugVue(fundebug)).use(store).use(router)
 app.mount('#app')
