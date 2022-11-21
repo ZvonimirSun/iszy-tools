cc77d6ce8e3e4b58dab6e407be29267c5839745b
ZvonimirSun <hi@iszy.xyz>
2022-11-21 14:39:15 +0800

    feat(geoJson): 添加图层列表控制

--- src/views/gis/geoJson/child/addService.vue
+++ src/views/gis/geoJson/child/addService.vue
@@ -1,5 +1,31 @@
 <template>
-  <a-form-item
+  <template v-if="layers.length">
+    <div class="layer-wrapper">
+      <div
+        v-for="(item,index) in layers"
+        :key="item.id"
+        class="layer-item"
+      >
+        <div
+          class="layer-item-title"
+          :title="item.name"
+        >
+          {{ item.name }}
+        </div>
+        <div
+          class="layer-item-url"
+          :title="item.url"
+        >
+          {{ item.url }}
+        </div>
+        <a-button @click="removeLayer(index)">
+          移除
+        </a-button>
+      </div>
+    </div>
+    <a-divider />
+  </template>
+  <a-form
     :colon="false"
     class="addService"
   >
@@ -36,7 +62,7 @@
         添加
       </a-button>
     </a-form-item>
-  </a-form-item>
+  </a-form>
 </template>
 
 <script>
@@ -44,6 +70,7 @@ import { tiledMapLayer } from '@/utils/iclient-leaflet/index.js'
 import { dynamicMapLayer as esriDynamicMapLayer } from 'esri-leaflet/src/Layers/DynamicMapLayer.js'
 import { tiledMapLayer as esriTiledMapLayer } from 'esri-leaflet/src/Layers/TiledMapLayer.js'
 import { CRS, tileLayer, Map, Control } from 'leaflet'
+import { v4 as uuid } from 'uuid'
 
 export default defineComponent({
   name: 'AddService',
@@ -52,10 +79,11 @@ export default defineComponent({
     layerControl: { type: Control.Layers, default: undefined }
   },
   data: () => ({
-    id: 1,
+    count: 0,
     name: '服务 1',
     url: '',
-    type: 'supermap_rest'
+    type: 'supermap_rest',
+    layers: []
   }),
   methods: {
     addService () {
@@ -65,8 +93,8 @@ export default defineComponent({
       const serviceUrl = this.url
       const serviceType = this.type
       const serviceName = this.name
-      this.id++
-      this.name = '服务 ' + this.id
+      this.count++
+      this.name = '服务 ' + this.count
       let layer
       try {
         switch (serviceType) {
@@ -107,6 +135,19 @@ export default defineComponent({
       }
       if (layer && this.layerControl) {
         this.layerControl.addOverlay(layer, serviceName)
+        this.layers.push({
+          name: serviceName,
+          url: serviceUrl,
+          layer,
+          id: uuid()
+        })
+      }
+    },
+    removeLayer (index) {
+      if (this.layers[index]?.layer && this.layerControl) {
+        this.map.removeLayer(this.layers[index]?.layer)
+        this.layerControl.removeLayer(this.layers[index]?.layer)
+        this.layers.splice(index, 1)
       }
     }
   }
@@ -128,5 +169,41 @@ export default defineComponent({
   .formBtnItem {
     text-align: right;
   }
+}
+
+.layer-wrapper {
+  width: 100%;
+  display: flex;
+  gap: .8rem;
+  flex-direction: column;
+  padding: .8rem 0 .8rem .8rem;
+}
+
+.layer-item {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  gap: .8rem;
+
+  &-title {
+    font-weight: bold;
+    font-size: 2.2rem;
+    width: 10rem;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+  &-url {
+    color: #999;
+    flex: 1;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+}
+
+.ant-divider {
+  margin: .8rem 0;
 }
 </style>
