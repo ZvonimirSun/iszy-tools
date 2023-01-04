03e3409044a7fe4bc84627ad7d106f75ce3ab4b2
ZvonimirSun <hi@iszy.xyz>
2023-01-04 19:22:22 +0800

    feat(md5): 支持文件md5计算

--- src/views/crypto/md5.vue
+++ src/views/crypto/md5.vue
@@ -3,11 +3,33 @@
     direction="vertical"
     style="width: 100%"
   >
-    <a-textarea
-      v-model:value="data1"
-      :rows="5"
-      placeholder="请输入要进行 MD5 编码的字符"
-    />
+    <a-tabs
+      v-model:activeKey="mode"
+      type="card"
+    >
+      <a-tab-pane
+        key="text"
+        tab="文本"
+      >
+        <a-textarea
+          v-model:value="data1"
+          :rows="5"
+          placeholder="请输入要进行 MD5 计算的字符"
+        />
+      </a-tab-pane>
+      <a-tab-pane
+        key="file"
+        tab="文件"
+      >
+        <a-upload
+          :file-list="fileList"
+          :before-upload="fileCompute"
+          @remove="fileRemove"
+        >
+          <a-button>选择文件</a-button>
+        </a-upload>
+      </a-tab-pane>
+    </a-tabs>
     <a-typography-title :level="4">
       结果
     </a-typography-title>
@@ -15,39 +37,97 @@
       v-model:value="data2"
       :rows="5"
       readonly
-      placeholder="MD5 编码的结果"
+      placeholder="MD5 计算的结果"
     />
   </a-space>
 </template>
 
-<script setup>
-import MD5 from 'crypto-js/md5.js'
+<script setup lang="ts">
+import SparkMD5 from 'spark-md5'
+import $msg from 'ant-design-vue/es/message'
+import type { Ref } from 'vue'
+import type { UploadProps } from 'ant-design-vue'
+import type { FileType } from 'ant-design-vue/es/upload/interface'
 
-const data1 = ref('')
-const data2 = ref('')
+const data1: Ref<string> = ref('')
+const data2: Ref<string> = ref('')
+const mode: Ref<string> = ref('text')
+const fileList: Ref<UploadProps['fileList']> = ref([])
 
-watch(data1, (val) => {
+watch(data1, async (val: string) => {
   if (!val) {
     data2.value = ''
   } else {
-    data2.value = encode(val)
+    data2.value = '计算中...'
+    data2.value = await encode(val)
   }
 })
 
-function encode (val) {
+function fileCompute (val: FileType) {
+  fileList.value = [val]
+  data2.value = '计算中...'
+  encode(val).then((result: string) => {
+    data2.value = result
+  })
+  return false
+}
+
+function fileRemove () {
+  fileList.value = []
+}
+
+function encode (val: string | File): Promise<string> {
   try {
-    const tmp = MD5(val).toString()
-    return `MD5 32位 大写: ${tmp.toUpperCase()}
+    if (typeof val === 'string') {
+      const tmp = SparkMD5.hash(val).toString()
+      return Promise.resolve(`MD5 32位 大写: ${tmp.toUpperCase()}
 MD5 32位 小写: ${tmp.toLowerCase()}
 MD5 16位 大写: ${tmp.substring(8, 24).toUpperCase()}
-MD5 16位 小写: ${tmp.substring(8, 24).toLowerCase()}`
+MD5 16位 小写: ${tmp.substring(8, 24).toLowerCase()}`)
+    } else {
+      return new Promise((resolve) => {
+        const blobSlice = File.prototype.slice
+        const file = val
+        const chunkSize = 2097152 // Read in chunks of 2MB
+        const chunks = Math.ceil(file.size / chunkSize)
+        let currentChunk = 0
+        const spark = new SparkMD5.ArrayBuffer()
+        const fileReader = new FileReader()
+        fileReader.onload = function () {
+          if (fileReader.result) {
+            spark.append(fileReader.result as ArrayBuffer) // Append array buffer
+            currentChunk++
+
+            if (currentChunk < chunks) {
+              loadNext()
+            } else {
+              const tmp = spark.end()
+              resolve(`MD5 32位 大写: ${tmp.toUpperCase()}
+MD5 32位 小写: ${tmp.toLowerCase()}
+MD5 16位 大写: ${tmp.substring(8, 24).toUpperCase()}
+MD5 16位 小写: ${tmp.substring(8, 24).toLowerCase()}`)
+            }
+          }
+        }
+
+        fileReader.onerror = function () {
+          resolve('计算失败')
+        }
+
+        function loadNext () {
+          const start = currentChunk * chunkSize
+          const end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize
+
+          fileReader.readAsArrayBuffer(blobSlice.call(file, start, end))
+        }
+
+        loadNext()
+      })
+    }
   } catch (e) {
-    this.$msg.error(e)
-    return ''
+    $msg.error((e as Error).message)
+    return Promise.resolve('计算失败')
   }
 }
 
 </script>
-
-<style scoped lang="scss">
-</style>
