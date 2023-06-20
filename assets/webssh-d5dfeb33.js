c7b6c922d0684be3661ae647105bb6fd8970e71f
ZvonimirSun <acc@iszy.xyz>
2023-06-20 09:35:15 +0000

    Deploying to deploy-beta from @ ZvonimirSun/iszy-tools@cc5e7d64bfb789f9b2b8bee0b083e3b269c11449 🚀

---
+++ assets/webssh-d5dfeb33.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,l as _}from"./index-bce8d3cc.js";const t={},c={src:"https://webssh.iszy.xyz/",class:"webssh",title:"webssh"};function o(n,a){return s(),_("iframe",c)}const l=e(t,[["render",o],["__scopeId","data-v-56210036"]]);export{l as default};
