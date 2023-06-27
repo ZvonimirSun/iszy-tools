181e543fe0c5d370cd64e263218bc0a804cd1f83
ZvonimirSun <hi@iszy.xyz>
2023-06-27 20:45:03 +0800

    feat(mock): 添加项目管理页

---
+++ src/views/program/mock/mock.vue
@@ -0,0 +1,327 @@
+<template>
+  <div
+    v-if="!selectedPrj"
+    class="prj-wrapper"
+  >
+    <div
+      w-full
+      class="prj-list"
+    >
+      <div
+        v-for="(prj, index) of prjs"
+        :key="index"
+        class="prj-item"
+      >
+        <div class="prj-title">
+          {{ prj.name }}
+        </div>
+        <div class="prj-path">
+          {{ prj.path }}
+        </div>
+        <div class="prj-desc">
+          {{ prj.description }}
+        </div>
+        <div class="prj-control">
+          <div
+            class="prj-control-btn prj-btn-open"
+            @click="selectedPrj=prj"
+          >
+            <i class="i-icon-park-outline-preview-open" />
+          </div>
+          <div
+            class="prj-control-btn prj-btn-edit"
+            @click="openEditPrjDialog(prj)"
+          >
+            <i class="i-icon-park-outline-edit-two" />
+          </div>
+          <el-popconfirm
+            title="确认要删除该项目吗？删除后无法恢复！"
+            @confirm="deletePrj(prj)"
+          >
+            <template #reference>
+              <div class="prj-control-btn prj-btn-delete">
+                <i class="i-icon-park-outline-delete" />
+              </div>
+            </template>
+          </el-popconfirm>
+        </div>
+      </div>
+      <div
+        class="prj-add"
+        @click="openCreatePrjDialog"
+      >
+        <i class="i-icon-park-outline-plus" />
+      </div>
+    </div>
+  </div>
+  <div
+    v-else
+    w-full
+    h-full
+  />
+  <el-dialog
+    v-model="showPrjDialog"
+    :title="form.id ? '修改项目' : '添加项目'"
+  >
+    <el-form :model="form">
+      <el-form-item
+        label="项目名称"
+        :label-width="120"
+        :required="true"
+      >
+        <el-input
+          v-model="form.name"
+          autocomplete="off"
+        />
+      </el-form-item>
+      <el-form-item
+        label="接口基础路径"
+        :label-width="120"
+        :required="true"
+      >
+        <el-input
+          v-model="form.path"
+          autocomplete="off"
+        />
+      </el-form-item>
+      <el-form-item
+        label="项目描述"
+        :label-width="120"
+      >
+        <el-input
+          v-model="form.desc"
+          type="textarea"
+          autocomplete="off"
+        />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="showPrjDialog = false">取消</el-button>
+        <el-button
+          type="primary"
+          @click="createOrEditPrj(form)"
+        >
+          提交
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+<script setup lang="ts">
+import type { Ref } from 'vue'
+import axios from '@/plugins/Axios'
+
+interface MockPrj {
+  id?: string
+  name: string
+  path: string
+  description?: string
+}
+
+interface MockData {
+  id?: number
+  name: string
+  type: 'post' | 'get'
+  enabled: boolean
+  path: string
+  description?: string
+  delay?: number
+  response?: string | object
+  projectId: string
+}
+
+const prjs: Ref<MockPrj[]> = ref([])
+const selectedPrj: Ref<MockPrj | null> = ref(null)
+
+const showPrjDialog: Ref<boolean> = ref(false)
+
+const form = reactive({
+  id: '',
+  name: '',
+  path: '',
+  description: ''
+})
+
+onMounted(() => {
+  getMockPrjs()
+})
+
+function openCreatePrjDialog () {
+  form.id = ''
+  form.name = ''
+  form.path = ''
+  form.description = ''
+  showPrjDialog.value = true
+}
+
+function openEditPrjDialog (prj: MockPrj) {
+  form.id = prj.id
+  form.name = prj.name
+  form.path = prj.path
+  form.description = prj.description
+  showPrjDialog.value = true
+}
+
+function createOrEditPrj (prj: MockPrj) {
+  if (prj.id) {
+    editPrj(prj)
+  } else {
+    createPrj(prj)
+  }
+}
+
+function createPrj (prj: MockPrj) {
+  axios.post(`${axios.$apiBase}/mock/api/prj`, { ...prj, id: undefined }).then(axios.getData).then((data: {
+    success: boolean
+  }) => {
+    if (data.success) {
+      ElMessage.success('创建项目成功')
+      getMockPrjs()
+      showPrjDialog.value = false
+    } else {
+      ElMessage.error('创建项目失败')
+    }
+  })
+}
+
+function editPrj (prj:MockPrj) {
+  axios.put(`${axios.$apiBase}/mock/api/prj/${prj.id}`, prj).then(axios.getData).then((data: {
+    success: boolean
+  }) => {
+    if (data.success) {
+      ElMessage.success('修改项目成功')
+      getMockPrjs()
+      showPrjDialog.value = false
+    } else {
+      ElMessage.error('修改项目失败')
+    }
+  })
+}
+
+async function deletePrj (prj: MockPrj) {
+  try {
+    await axios.delete(`${axios.$apiBase}/mock/api/prj/${prj.id}`)
+    ElMessage.success('删除项目成功')
+  } catch (e) {
+    ElMessage.error('删除项目失败')
+  }
+  getMockPrjs()
+}
+
+function getMockPrjs () {
+  axios.get(`${axios.$apiBase}/mock/api/prj/list`).then(axios.getData).then((data: {
+    success: boolean
+    data?: MockPrj[]
+  }) => {
+    if (data.success) {
+      prjs.value = data.data || []
+    } else {
+      ElMessage.error('获取项目列表失败')
+    }
+  })
+}
+</script>
+
+<style scoped lang="scss">
+:deep(.el-dialog) {
+ @media screen and (max-width: 768px) {
+    --el-dialog-width: 90% !important;
+  }
+}
+
+.prj-wrapper {
+  height: 100%;
+  width: 100%;
+  overflow: auto;
+}
+
+.prj-list {
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+}
+
+.prj-item, .prj-add {
+  background: var(--el-fill-color-blank);
+  box-shadow: var(--el-box-shadow-light);
+  border-radius: var(--el-border-radius-middle);
+  width: 30rem;
+  height: 20rem;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+}
+
+.prj-add {
+  justify-content: center;
+  align-items: center;
+  font-size: 8rem;
+  color: var(--el-text-color-secondary);
+  cursor: pointer;
+  transition: all .3s;
+
+  &:hover {
+    background-color: var(--el-fill-color-light);
+  }
+}
+
+.prj-title {
+  font-size: 1.6rem;
+  font-weight: bold;
+  padding: 0 2rem;
+  height: 4rem;
+  line-height: 4rem;
+  background-color: var(--el-fill-color);
+}
+
+.prj-path {
+  padding: 0 2rem;
+  font-weight: bold;
+  height: 2rem;
+  line-height: 2.2rem;
+  font-size: 1.6rem;
+}
+
+.prj-desc {
+  flex: 1;
+  padding: 1rem 2rem;
+  color: var(--el-text-color-secondary);
+}
+
+.prj-control {
+  height: 3rem;
+  display: flex;
+  border-top: solid .1rem var(--el-border-color);
+
+  .prj-control-btn {
+    width: calc(100% / 3);
+    text-align: center;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    cursor: pointer;
+
+    font-size: 1.8rem;
+    transition: all .3s;
+
+    &:hover {
+      background-color: var(--el-fill-color-light);
+    }
+  }
+
+  .prj-btn-open {
+    color: var(--el-color-primary);
+  }
+
+  .prj-btn-delete {
+    color: var(--el-color-danger);
+  }
+}
+</style>
