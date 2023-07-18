2c42facebc02ba25fef1eca5a601b82d72a6b43e
ZvonimirSun <acc@iszy.xyz>
2023-07-18 16:58:08 +0000

    Deploying to deploy from @ ZvonimirSun/iszy-tools@674dabc4189ce6446ef924e436598c73f1ef05a6 🚀

---
+++ assets/Paragraph-192089fa.js
@@ -0,0 +1,1 @@
+import{b as o,B as i,o as g,g as m}from"./index-ceb39801.js";import{a as c}from"./index-e710c703.js";var h=function(){return g(m(),["component"])},a=function(s,r){var t=r.slots,n=r.attrs,e=o(o({},s),{},{component:"div"},n);return c(i,e,t)};a.displayName="ATypographyParagraph";a.inheritAttrs=!1;a.props=h();const l=a;export{l as _};
