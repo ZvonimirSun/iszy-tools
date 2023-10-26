db82231b344a51d5ee92c058ee93b37314968d96
ZvonimirSun <hi@iszy.xyz>
2023-10-26 16:19:25 +0800

    feat(nzh): 添加数字中文转换工具

---
+++ src/views/others/nzh.vue
@@ -0,0 +1,75 @@
+<template>
+  <el-form label-position="top">
+    <el-form-item label="小写">
+      <el-input
+        v-model="num"
+        @change="fromNum"
+      />
+    </el-form-item>
+    <el-form-item label="中文小写">
+      <el-input
+        v-model="s"
+        @change="fromS"
+      />
+    </el-form-item>
+    <el-form-item label="中文大写">
+      <el-input
+        v-model="b"
+        @change="fromB"
+      />
+    </el-form-item>
+    <el-form-item label="中文金额">
+      <el-input
+        v-model="money"
+        readonly
+      />
+    </el-form-item>
+  </el-form>
+</template>
+
+<script setup lang="ts">
+import type { Ref } from 'vue'
+import Nzh from 'nzh'
+
+const money: Ref<string> = ref('')
+const s: Ref<string> = ref('')
+const b: Ref<string> = ref('')
+const num: Ref<string> = ref('')
+
+function fromNum (num: string) {
+  if (!num) {
+    return
+  }
+  try {
+    s.value = Nzh.cn.encodeS(num)
+    b.value = Nzh.cn.encodeB(num)
+    money.value = Nzh.cn.toMoney(num)
+  } catch (e) {}
+}
+
+function fromS (s: string) {
+  if (!s) {
+    return
+  }
+  try {
+    num.value = Nzh.cn.decodeS(s)
+    b.value = Nzh.cn.encodeB(num.value)
+    money.value = Nzh.cn.toMoney(num.value)
+  } catch (e) {}
+}
+
+function fromB (b: string) {
+  if (!b) {
+    return
+  }
+  try {
+    num.value = Nzh.cn.decodeB(b)
+    s.value = Nzh.cn.encodeS(num.value)
+    money.value = Nzh.cn.toMoney(num.value)
+  } catch (e) {}
+}
+</script>
+
+<style scoped>
+
+</style>
