ffebd576f1335543eadc7f25dded13f912b6e570
ZvonimirSun <hi@iszy.xyz>
2023-12-19 01:25:50 +0800

    feat(javascript): 添加lint

--- src/components/editor/javascript.ts
+++ src/components/editor/javascript.ts
@@ -1,12 +1,31 @@
-import { javascript } from '@codemirror/lang-javascript'
+import { javascript, esLint } from '@codemirror/lang-javascript'
+import { linter, lintGutter } from '@codemirror/lint'
 import basic from './basic'
 import { EditorPlugin } from './editor'
+
+// Uses linter.mjs
+import * as eslint from 'eslint-linter-browserify'
+
+const config = {
+  // eslint configuration
+  parserOptions: {
+    ecmaVersion: 15,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true
+  }
+}
 
 const plugin: EditorPlugin = {
   ...basic,
   extensions: [
     ...basic.extensions,
-    javascript()
+    javascript(),
+    lintGutter(),
+    // eslint-disable-next-line
+    linter(esLint(new eslint.Linter(), config)),
   ]
 }
 
