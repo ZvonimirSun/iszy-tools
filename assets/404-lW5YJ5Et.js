b329ac5ff6a7b4f5c15b6efdb30b929adde62eb7
ZvonimirSun <acc@iszy.xyz>
2023-12-25 02:57:14 +0000

    Deploying to deploy from @ ZvonimirSun/iszy-tools@7b605b1ddeae256974bdebf335119c3956d6e6c7 🚀

---
+++ assets/404-lW5YJ5Et.js
@@ -0,0 +1,1 @@
+import{a3 as t,ay as c,o,b3 as a}from"./vendor-ZyFjMaVl.js";const n={};function s(_,r){const e=a;return o(),c(e,{icon:"warning",title:"404","sub-title":"对不起，你访问的页面不存在。"})}const i=t(n,[["render",s],["__scopeId","data-v-854ab15b"]]);export{i as default};
--- assets/webssh-Ai7nP9kA.js
+++
@@ -1,1 +0,0 @@
-import{a3 as s,c as e,o as c}from"./vendor-nejBBORZ.js";const t={},o={src:"https://webssh.iszy.xyz/",class:"webssh",title:"webssh"};function a(r,_){return c(),e("iframe",o)}const f=s(t,[["render",a],["__scopeId","data-v-c95b8251"]]);export{f as default};
