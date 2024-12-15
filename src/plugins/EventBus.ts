a6ab4993e33c6ef5eb5918bb54806a3a31c324bc
zvonimirsun <hi@iszy.xyz>
2024-12-15 22:34:48 +0800

    feat: 替换EventBus为mitt

--- src/plugins/EventBus.ts
+++ src/plugins/EventBus.ts
@@ -1,43 +1,8 @@
-export default class EventBus {
-  static _events: Map<string, ((...payload: any[]) => any)[]> = new Map()
+import type { EventType } from 'mitt'
+import mitt from 'mitt'
 
-  static on(name: string, event: (...payload: any[]) => any) {
-    if (name && typeof event === 'function') {
-      const tmp = this._events.get(name)
-      if (tmp) {
-        tmp.push(event)
-      }
-      else {
-        this._events.set(name, [event])
-      }
-    }
-  }
+export default mitt()
 
-  static off(name: string, event?: (...payload: any[]) => any) {
-    const tmp = this._events.get(name)
-    if (tmp) {
-      if (typeof event === 'function') {
-        for (let i = tmp.length - 1; i >= 0; i--) {
-          if (event === tmp[i]) {
-            tmp.splice(i, 1)
-          }
-        }
-        if (tmp.length === 0) {
-          this._events.delete(name)
-        }
-      }
-      else {
-        this._events.delete(name)
-      }
-    }
-  }
-
-  static emit(name: string, ...payload: any[]) {
-    const tmp = this._events.get(name)
-    if (tmp) {
-      for (const fn of tmp) {
-        fn(...payload)
-      }
-    }
-  }
+export function getEventBus<T extends Record<EventType, any>>() {
+  return mitt<T>()
 }
