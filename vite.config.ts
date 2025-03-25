403b6b62c04377f14a9fa3b241f634010163be0c
ZvonimirSun <hi@iszy.xyz>
2025-03-25 12:42:46 +0800

    chore: 修复unocss inspector不显示问题，规避和vite-plugin-html的冲突

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 'use strict'
+import type { UserConfig } from 'vite'
 import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -8,36 +9,165 @@ import { AntDesignVueResolver, ElementPlusResolver } from 'unplugin-vue-componen
 import Components from 'unplugin-vue-components/vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import { VitePWA } from 'vite-plugin-pwa'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import { defineConfig } from 'vitest/config'
 import config from './src/config'
 import Sitemap from './src/plugins/Sitemap.js'
 import tools from './src/tools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-    strictPort: true,
-    hmr: {
+export default defineConfig(({ command }) => {
+  const viteConfig: UserConfig = {
+    server: {
       port: 3000,
-    },
-    proxy: {
-      '/api': {
-        target: config.apiOrigin,
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+      strictPort: true,
+      hmr: {
+        port: 3000,
+      },
+      proxy: {
+        '/api': {
+          target: config.apiOrigin,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-  plugins: [
-    vue(),
-    vueJsx(),
-    createHtmlPlugin({
-      entry: 'src/main.ts',
-      inject: {
-        data: {
-          ...config,
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools(),
+      AutoImport({
+        resolvers: [
+          ElementPlusResolver(),
+        ],
+        imports: [
+          'vue',
+          'vue-router',
+          'pinia',
+          '@vueuse/core',
+        ],
+        eslintrc: {
+          enabled: true,
         },
+        dirs: ['src/composables', 'src/stores'],
+        vueTemplate: true,
+        dts: 'src/types/auto-imports.d.ts',
+      }),
+      Unocss(),
+      Components({
+        include: [/\.vue$/, /\.vue\?vue/],
+        dirs: ['src/components'],
+        extensions: ['vue'],
+        dts: 'src/types/components.d.ts',
+        resolvers: [
+          ElementPlusResolver(),
+          AntDesignVueResolver({
+            importStyle: false,
+          }),
+        ],
+      }),
+      VitePWA({
+        base: '/',
+        strategies: 'generateSW',
+        registerType: 'prompt',
+        manifest: {
+          id: config.name,
+          name: config.zhName,
+          short_name: config.alias,
+          theme_color: '#ffffff',
+        },
+        pwaAssets: {
+          config: true,
+          overrideManifestIcons: true,
+        },
+        workbox: {
+          runtimeCaching: [
+            // api
+            {
+              urlPattern: ({ url }) => url.origin === 'https://api.iszy.xyz',
+              handler: 'NetworkFirst',
+              options: {
+                cacheName: `${config.key}-api`,
+                cacheableResponse: {
+                  statuses: [200],
+                },
+              },
+            },
+            // cdn
+            {
+              urlPattern: /^https:\/\/.*cdn\.iszy\.xyz/,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: `${config.key}-cdn`,
+                expiration: {
+                  maxEntries: 30,
+                  maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
+              },
+            },
+            // cdn
+            {
+              urlPattern: /^https:\/\/.*cdn\.iszy\.cc/,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: `${config.key}-cdn`,
+                expiration: {
+                  maxEntries: 30,
+                  maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
+              },
+            },
+          ],
+        },
+      }),
+      Sitemap({ tools, hostname: config.siteOrigin }),
+    ],
+    optimizeDeps: {
+      include: ['vue', 'element-plus'],
+    },
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          charset: false,
+          api: 'modern-compiler',
+        },
+        less: {
+          javascriptEnabled: true,
+        },
+      },
+    },
+    build: {
+      rollupOptions: {
+        onwarn(warning, warn) {
+          // Suppress "Module level directives cause errors when bundled" warnings
+          if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+            return
+          }
+          warn(warning)
+        },
+      },
+    },
+    worker: {
+      format: 'es',
+    },
+    test: {
+      environment: 'jsdom',
+    },
+  }
+  if (command === 'build') {
+    viteConfig.plugins?.push(createHtmlPlugin({
+      inject: {
         tags: [
           {
             injectTo: 'head',
@@ -63,133 +193,7 @@ export default defineConfig({
           },
         ],
       },
-    }),
-    AutoImport({
-      resolvers: [
-        ElementPlusResolver(),
-      ],
-      imports: [
-        'vue',
-        'vue-router',
-        'pinia',
-        '@vueuse/core',
-      ],
-      eslintrc: {
-        enabled: true,
-      },
-      dirs: ['src/composables', 'src/stores'],
-      vueTemplate: true,
-      dts: 'src/types/auto-imports.d.ts',
-    }),
-    Unocss(),
-    Components({
-      include: [/\.vue$/, /\.vue\?vue/],
-      dirs: ['src/components'],
-      extensions: ['vue'],
-      dts: 'src/types/components.d.ts',
-      resolvers: [
-        ElementPlusResolver(),
-        AntDesignVueResolver({
-          importStyle: false,
-        }),
-      ],
-    }),
-    VitePWA({
-      base: '/',
-      strategies: 'generateSW',
-      registerType: 'prompt',
-      manifest: {
-        id: config.name,
-        name: config.zhName,
-        short_name: config.alias,
-        theme_color: '#ffffff',
-      },
-      pwaAssets: {
-        config: true,
-        overrideManifestIcons: true,
-      },
-      workbox: {
-        runtimeCaching: [
-          // api
-          {
-            urlPattern: ({ url }) => url.origin === 'https://api.iszy.xyz',
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: `${config.key}-api`,
-              cacheableResponse: {
-                statuses: [200],
-              },
-            },
-          },
-          // cdn
-          {
-            urlPattern: /^https:\/\/.*cdn\.iszy\.xyz/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: `${config.key}-cdn`,
-              expiration: {
-                maxEntries: 30,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-          // cdn
-          {
-            urlPattern: /^https:\/\/.*cdn\.iszy\.cc/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: `${config.key}-cdn`,
-              expiration: {
-                maxEntries: 30,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-        ],
-      },
-    }),
-    Sitemap({ tools, hostname: config.siteOrigin }),
-  ],
-  optimizeDeps: {
-    include: ['vue', 'element-plus'],
-  },
-  resolve: {
-    alias: {
-      '@': resolve('src'),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        charset: false,
-        api: 'modern-compiler',
-      },
-      less: {
-        javascriptEnabled: true,
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        // Suppress "Module level directives cause errors when bundled" warnings
-        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
-          return
-        }
-        warn(warning)
-      },
-    },
-  },
-  worker: {
-    format: 'es',
-  },
-  test: {
-    environment: 'jsdom',
-  },
+    }))
+  }
+  return viteConfig
 })
