af1557dd0add7352550138e4c33226061dd88aa7
zvonimirsun <hi@iszy.xyz>
2025-04-01 22:04:51 +0800

    chore: 暂时移除不兼容的sitemap插件

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { defineConfig } from 'vitest/config'
 import config from './src/config'
-import Sitemap from './src/plugins/Sitemap.js'
-import tools from './src/tools'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -119,7 +117,6 @@ export default defineConfig(({ command }) => {
           ],
         },
       }),
-      Sitemap({ tools, hostname: config.siteOrigin }),
     ],
     optimizeDeps: {
       include: ['vue', 'element-plus'],
